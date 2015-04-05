8e98ea9920b15e59212aac6cb32b39d2ea5e1c04
Chanel Huynh <chanelhuynh@ucla.edu>
2015-04-05 14:09:00 -0700

    Update main.c

--- main.c
+++ main.c
@@ -2,28 +2,26 @@
  * main.c
  * Sets up the Window, ClickConfigProvider and ClickHandlers.
  */
-
-
+ 
 #include <pebble.h>
 
 static Window *s_main_window_i;    // Input
-static Window *s_main_window_t;   // Notes + Count down
+static Window *s_main_window_t;   // Notes + Countdown
 
-static TextLayer *s_output_layer_i;  // 
-static TextLayer *s_output_layer_t;  // 
+static TextLayer *s_output_layer_i; 
+static TextLayer *s_output_layer_t;
 
 static AppTimer *s_timer_half;
 static AppTimer *s_timer_min;
 
 static char s_uptime_buffer[32];
 
-
-// nandini
+// Nandini
 int counter = 0;
 static Window *s_main_window_n;
 static TextLayer *s_output_layer_n;
 
-// alex
+// Alex
 static void f_init();
 static void update_text();
 #define REPEAT_INTERVAL_MS 50
@@ -38,8 +36,7 @@ static GBitmap *s_icon_plus, *s_icon_minus;
 
 static int s_num_fidgets = NUM_FIDGETS_DEFAULT;
 
-
-//end alex
+// End Alex
 static int min = 0;
 static int sec = 0;    // make sure it doesnt go over 60
 static int saved_min = 0;
@@ -50,7 +47,7 @@ static int warn_min = 0;
 
 static int state = 0;  // Like a state machine
 
-// Notecard
+// Notecards
 void update_Notecard(int counter) {
   switch(counter) {
     case 0:
@@ -68,21 +65,18 @@ void update_Notecard(int counter) {
   }
 }
 
-
-
 static void main_window_load_n(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect window_bounds = layer_get_bounds(window_layer);
-  // Create output TextLayer
+  
+  // Create output textLayer
   s_output_layer_n = text_layer_create(GRect(5, 0, window_bounds.size.w-10, window_bounds.size.h/2));
-  //FONT_MM_24 = fonts_load_custom_font(resource_get_handle(FONT_MM_24));
-  //text_layer_set_font(s_font_layer, FONT_MM_24);
   text_layer_set_overflow_mode(s_output_layer_n, GTextOverflowModeWordWrap);
   layer_add_child(window_layer, text_layer_get_layer(s_output_layer_n));
 }
 
 static void main_window_unload_n(Window *window) {
-  // Destroy output TextLayer
+  // Destroy output textLayer
   text_layer_destroy(s_output_layer_n);
 }
 
@@ -103,13 +97,8 @@ void up_single_click_handler_n(ClickRecognizerRef recognizer, void *context) {
 
 void config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_DOWN, down_single_click_handler_n);
-  //window_single_click_subscribe(BUTTON_ID_SELECT, select_single_click_handler);
   window_single_click_subscribe(BUTTON_ID_UP, up_single_click_handler_n);
 }
-
-
-
-
 
 // Input
 static void printInput() {
@@ -147,7 +136,7 @@ static void up_long_click_handler(ClickRecognizerRef recognizer, void *context) 
 
 static void down_long_click_handler(ClickRecognizerRef recognizer, void *context) {
   
-  if (state)   // Sub 10
+  if (state) // Sub 10
     sec = sec - 10;
   else 
     min = min - 10;
@@ -157,14 +146,12 @@ static void down_long_click_handler(ClickRecognizerRef recognizer, void *context
   printInput();
 }
 
-
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
   (state) ? sec-- : min--;
   if (sec < 0) sec = 0;
   if (min < 0) min = 0;
   printInput();
 }
-
 
 static void timer_callback_half(void *data) {
   vibes_double_pulse();
@@ -177,14 +164,13 @@ static void timer_callback_min(void *data) {
   text_layer_set_text(s_output_layer_t, "1 minute left!");
 }
 
-
 static void tick_handler(struct tm* tick_time, TimeUnits units_changed) {
  
   if (min == 0 && sec == 0) {
     tick_timer_service_unsubscribe();
     light_enable(true);
     vibes_long_pulse();
-    psleep(2000);      // pebble guy: use apptimer instead if it screws up other stuff
+    psleep(2000); // Pebble guy says to use apptimer instead if it messes up other stuff
     text_layer_set_text(s_output_layer_t, "Time's up!");
     psleep(2000);
     window_stack_pop(true);
@@ -204,7 +190,7 @@ static void tick_handler(struct tm* tick_time, TimeUnits units_changed) {
   }
 }
 
-//Alex
+// Alex
 static void tap_handler(AccelAxisType axis, int32_t direction) {
   s_num_fidgets++;
 }
@@ -219,7 +205,7 @@ static void main_window_load_f(Window *window) {
   text_layer_set_text(s_header_layer, "Fidget Counter");
   layer_add_child(window_layer, text_layer_get_layer(s_header_layer));
 
-  //Layer for Text
+  //Layer for text
   s_body_layer = text_layer_create(GRect(4, 44, width, 60));
   text_layer_set_font(s_body_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
   text_layer_set_background_color(s_body_layer, GColorClear);
@@ -240,20 +226,13 @@ static void update_text() {
   snprintf(s_body_text, sizeof(s_body_text), "You had %u", s_num_fidgets/2);
   text_layer_set_text(s_body_layer, s_body_text);
 }
-
-
-//end alex
-
+// End Alex
 
 static void main_window_load_t(Window *window) {
-  //Layer *note_layer = window_get_root_layer(window);
-  
   Layer *window_layer = window_get_root_layer(window);
   GRect window_bounds = layer_get_bounds(window_layer);
   
-  //s_output_layer_t = text_layer_create(GRect(5, 0, window_bounds.size.w - 10 ,window_bounds_size.h/2));
-  
-  // add timer layer
+  // Add timer layer
   s_output_layer_t = text_layer_create(GRect(5, 5, window_bounds.size.w - 10 ,window_bounds.size.h/4));
   text_layer_set_text(s_output_layer_t, "Starting timer...");
   text_layer_set_font(s_output_layer_t, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
@@ -263,29 +242,22 @@ static void main_window_load_t(Window *window) {
   text_layer_set_text_alignment(s_output_layer_t, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_output_layer_t));
 
-  // add notecard layer
+  // Add notecard layer
   s_output_layer_n = text_layer_create(GRect(5, window_bounds.size.h/4, window_bounds.size.w-10, window_bounds.size.h));
-  //FONT_MM_24 = fonts_load_custom_font(resource_get_handle(FONT_MM_24));
-  //text_layer_set_font(s_font_layer, FONT_MM_24);
   text_layer_set_overflow_mode(s_output_layer_n, GTextOverflowModeWordWrap);
   layer_add_child(window_layer, text_layer_get_layer(s_output_layer_n));
    
-  // ADD TIMER  
+  // Add timer 
   s_timer_half = app_timer_register(warn_half, timer_callback_half, NULL);
   s_timer_min = app_timer_register(warn_min, timer_callback_min, NULL);
   tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
-  
-
 }
 
 static void main_window_unload(Window *window) {
-  text_layer_destroy(s_output_layer_t); // Destroy output TextLayer
+  text_layer_destroy(s_output_layer_t); // Destroy output textLayer
 }
 
-
-
 static void select_long_click_handler(ClickRecognizerRef recognizer, void *context) {
-  
   // Set status
   state = 2;
   saved_min = min;
@@ -295,16 +267,17 @@ static void select_long_click_handler(ClickRecognizerRef recognizer, void *conte
   millis = 1000 * (min * 60 + sec);  // Timer time in milliseconds
   warn_half = millis / 2;            // Timer warning at half
   warn_min = millis - 60000;         // Timer warning at 1 min left
-  //Alex put this here
+  
+  // Alex put this here
   accel_tap_service_subscribe(tap_handler);
+  
   // Push second window
   window_set_fullscreen(s_main_window_t, true);
   window_stack_push(s_main_window_t, true);
 }
-             
 
 static void click_config_provider(void *context) {
-  // Register the ClickHandlers -- Short click
+  // Register the ClickHandlers - Short click
   window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
@@ -317,14 +290,13 @@ static void main_window_load_i(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect window_bounds = layer_get_bounds(window_layer);
 
-  // Create output TextLayer
+  // Create output textLayer
   s_output_layer_i = text_layer_create(GRect(5, 0, window_bounds.size.w - 10, window_bounds.size.h));
   text_layer_set_font(s_output_layer_i, fonts_get_system_font(FONT_KEY_GOTHIC_24));
   printInput();
 
   text_layer_set_overflow_mode(s_output_layer_i, GTextOverflowModeWordWrap);
   layer_add_child(window_layer, text_layer_get_layer(s_output_layer_i));
-  
 }
 
 static void main_input_init() {
@@ -332,26 +304,28 @@ static void main_input_init() {
   sec = saved_sec;
   printInput();
 }
-//Alex
+
+// Alex
 static void f_init() {
   s_main_window = window_create();
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = main_window_load_f,
     .unload = main_window_unload,
   });
-  //subscribe to taps
+  // Subscribe to taps
   
   window_stack_push(s_main_window, true);
 }
 
 static void init() {
-  // Create input Window
+  // Create input window
   s_main_window_i = window_create();
   window_set_window_handlers(s_main_window_i, (WindowHandlers) {
     .load = main_window_load_i,
     .unload = main_window_unload,
     .appear = main_input_init
   });
+  
   window_set_click_config_provider(s_main_window_i, click_config_provider);
   
   // Second set up window
@@ -376,4 +350,3 @@ int main() {
   app_event_loop();
   deinit();
 }
-
